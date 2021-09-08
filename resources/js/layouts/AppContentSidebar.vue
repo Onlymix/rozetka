24398158c1a9d1c391441b7bf3869639f2d67ca0
DaRK <vlad94@ukr.net>
2021-09-08 07:11:53 +0300

    main sidebar

--- resources/js/layouts/AppContentSidebar.vue
+++ resources/js/layouts/AppContentSidebar.vue
@@ -1,6 +1,5 @@
 <template>
     <aside style="width: 300px">
-        <!--region done-->
         <q-list class="text-blue-8 q-mt-lg">
             <q-item v-for="item in menuLinks" :key="item.id" clickable manual-focus :to="item.url" dense>
                 <q-item-section avatar class="col-shrink no-padding">
@@ -25,8 +24,8 @@
         <q-separator color="grey-12" />
         <div class="apps row q-gutter-md sidebar-padding">
             <span class="text-grey-6 col-12">Устанавливайте наши приложения</span>
-            <a href="#"><img alt="Google Play" src="https://xl-static.rozetka.com.ua/assets/img/design/button-google-play-ru.svg" /></a>
-            <a href="#"><img alt="AppStore" src="https://xl-static.rozetka.com.ua/assets/img/design/button-appstore-ru.svg" /></a>
+            <a href="#"><svg-icon icon-id="icon-apps-GooglePlay" w="96px" h="32px" /></a>
+            <a href="#"><svg-icon icon-id="icon-apps-AppStore" w="96px" h="32px" /></a>
         </div>
         <q-separator color="grey-12" />
         <div class="sidebar-padding row q-gutter-sm items-center">
@@ -62,16 +61,84 @@
             </template>
         </q-list>
         <q-separator color="grey-12" />
-        <!--endregion-->
-        <div class="copyright flex sidebar-padding q-gutter-sm">
+        <div class="flex copyright sidebar-padding q-gutter-y-sm q-gutter-x-md justify-center">
+            <q-btn unelevated class="no-padding" :ripple="false" @click="masterCard = !masterCard">
+                <svg-icon icon-id="icon-payments-MasterCard" w="72px" h="32px" />
+            </q-btn>
+            <q-btn unelevated class="no-padding" :ripple="false" @click="visa = !visa">
+                <svg-icon icon-id="icon-payments-Visa" w="72px" h="32px" />
+            </q-btn>
             <span class="text-grey-6">ТМ используется на основании лицензии правообладателя RozetkaLTD.</span>
             <span>© Интернет-магазин «Розетка™» 2001–2021</span>
         </div>
+        <q-dialog v-model="masterCard">
+            <q-card style="min-width: 640px">
+                <q-card-section class="row items-center q-pb-none">
+                    <div class="text-h6">MasterCard® SecureCode™</div>
+                    <q-space />
+                    <q-btn v-close-popup icon="close" flat round dense />
+                </q-card-section>
+
+                <q-separator color="grey-4" class="q-mt-sm" />
+
+                <q-card-section>
+                    MasterCard® SecureCode™ — это технология, которая обеспечивает Вашу карту MasterCard® или Maestro® дополнительным
+                    секретным кодом, защищающим ее от несанкционированного использования во время совершения покупок в интернет-магазинах,
+                    участвующих в программе.
+                </q-card-section>
+                <q-card-section>
+                    Чтобы подключить технологию SecureCode, необходимо обратиться в банк, выпустивший Вашу карту. Ваш секретный код будет
+                    известен только Вам и Вашему банку. Далее при каждой оплате покупок или услуг в интернет-магазинах, участвующих в
+                    программе, Ваш банк предложит Вам ввести данный секретный код, который Вы получите на Ваш мобильный телефон,
+                    зарегистрированный в банке. Только после проверки и подтверждения банком данного кода операция по оплате будет
+                    произведена. Процедура использования технологии SecureCode так же проста, как введение PIN-кода в банкомате.
+                </q-card-section>
+                <q-card-section>
+                    Введя правильный SecureCode в процессе покупки, Вы подтверждаете, что являетесь авторизованным владельцем карты. Если
+                    введен неправильный SecureCode, покупка не будет совершена. Даже если кому-то известен номер Вашей платежной карты, без
+                    SecureCode не удастся оплатить покупку в интернет-магазинах, участвующих в программе.
+                </q-card-section>
+            </q-card>
+        </q-dialog>
+        <q-dialog v-model="visa">
+            <q-card style="min-width: 640px">
+                <q-card-section class="row items-center q-pb-none">
+                    <div class="text-h6">Verified by Visa</div>
+                    <q-space />
+                    <q-btn v-close-popup icon="close" flat round dense />
+                </q-card-section>
+
+                <q-separator color="grey-4" class="q-mt-sm" />
+
+                <q-card-section>
+                    Платежные системы в партнерстве с банками-эмитентами внедряют современные схемы проверки личности держателя карты, чтобы
+                    сделать покупки в Интернете более безопасными. Устанавливается специальный пароль для каждой совершаемой операции, и это
+                    вселяет в Вас уверенность, что только Вы можете делать такие покупки онлайн. Современные технологические решения
+                    необходимы для того, чтобы держатель карты был уверен в безопасности транзакции и в том, что он имеет дело с настоящим
+                    (а не «подставным») магазином.
+                </q-card-section>
+                <q-card-section> Одно из таких технологических решений называется Verified by Visa («Проверено Visa»). </q-card-section>
+                <q-card-section>
+                    Verified by Visa — это новая система защиты, которая извещает онлайн-торговцев, участвующих в программе, и банки о том,
+                    что Вы являетесь настоящим владельцем карточки, когда Вы совершаете онлайн-покупки. Она позволяет Вам использовать
+                    персональный пароль для подтверждения Вашей личности и защиты Вашей карточки Visa, когда Вы используете ее в Интернете,
+                    вселяя большую уверенность в совершаемых Вами действиях.
+                </q-card-section>
+                <q-card-section>
+                    К технологии Verified by Visa легко подключиться, ее легко использовать, услуга действует для всех карточек Visa.
+                </q-card-section>
+                <q-card-section> Для активации услуги Verified by Visa для Вашей карты необходимо обратиться в Ваш банк. </q-card-section>
+            </q-card>
+        </q-dialog>
     </aside>
 </template>
 
 <script>
+import SvgIcon from '../components/SvgIcon'
+import { ref } from 'vue'
+
 export default {
+    components: { SvgIcon },
     setup() {
         const supportLinks = [
             {
@@ -171,7 +238,7 @@ export default {
                 style: 'background-color: #08c',
             },
         ]
-        return { menuLinks, socialLinks, supportLinks }
+        return { menuLinks, socialLinks, supportLinks, masterCard: ref(false), visa: ref(false) }
     },
 }
 </script>
@@ -189,4 +256,6 @@ export default {
     height 32px
 .copyright
     font-size 0.9em
+.q-card .q-card__section:nth-child(n+4)
+    padding-top 0 !important
 </style>
